aaac8adf6a8e76150fda6b402e5d2c83aeb3e7f1
Sunera Chandeepa Sandaruwan <chandeepasunera@gmail.com>
2025-02-02 16:38:52 +0530

    1.0.0 - main file create

---
+++ main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+import 'main_position.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const WelcomeScreen());
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(    
+        body: BackgroundImage(),
+      
+      ),     
+    );
+  }
+}
+
+class BackgroundImage extends StatelessWidget {
+  const BackgroundImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("images/Welcome.jpg"),
+          fit: BoxFit.fill, // Ensure the background image fills the entire screen
+        ),
+      ),
+      child: Stack(
+        children: [
+          // Get Start button using DecoratedBox
+          Align(
+            alignment: const Alignment(0.0, 0.6), // Position the DecoratedBox button
+            child: GestureDetector(
+              onTap: () {
+                // Navigate to the main position screen when the button is pressed
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Position()), // Navigate to the main position widget
+                );
+              },
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFE38C5A), // Updated button background color
+                  borderRadius: BorderRadius.circular(30), // Increased radius for more rounded corners
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      blurRadius: 5,
+                      offset: const Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 40),
+                  child: Text(
+                    'Get Start',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold, // Makes the text bold
+                      fontStyle: FontStyle.italic, // Makes the text italic
+                      color: Colors.black, // Text color
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
